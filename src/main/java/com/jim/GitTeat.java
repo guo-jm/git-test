51a3e9aa9ce37be9aa61ec6fc65751bb36460015
Jim <jim@com>
2022-03-28 11:35:48 +0800

    second commit

--- src/main/java/com/jim/GitTeat.java
+++ src/main/java/com/jim/GitTeat.java
@@ -3,5 +3,6 @@ package com.jim;
 public class GitTeat {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git2!");
     }
 }
